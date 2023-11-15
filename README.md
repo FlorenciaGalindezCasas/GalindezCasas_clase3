427900d9ec849cad58615f3a190fbe16b0356581
FlorenciaDGC <florenciagalindezcasas@hotmail.com>
2023-11-15 18:10:03 -0300

    inicial commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# GalindezCasas_clase3
---
+++ myapp/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Footer from './Componentes/Footer/Footer';
+import Header from './Componentes/Header/Header'
+import Producto from './Componentes/Producto/Producto';
+function App() {
+  return (
+    <>
+      <Header/>
+      <Producto/>
+      <Footer/>
+    </>
+  );
+}
+
+export default App
